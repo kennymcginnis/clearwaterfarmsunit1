f18d08590aee8f6626a0facf160d419a2251f4f2
Kenny McGinnis <kenneth.j.mcginnis@gmail.com>
2024-01-13 08:02:10 -0700

    adding user link on main irrigiation menu

--- app/components/main-nav.tsx
+++ app/components/main-nav.tsx
@@ -81,18 +81,20 @@ export function MainNavigationMenu() {
 						<ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
 							<li className="row-span-3">
 								<NavigationMenuLink asChild>
-									<div className="flex h-full w-full select-none flex-col items-center justify-center rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md">
-										<div className="flex flex-row">
-											<Droplet className="h-8 w-8" aria-hidden="true" />
-											<Droplets className="h-8 w-8" aria-hidden="true" />
+									<Link to="/users">
+										<div className="flex h-full w-full select-none flex-col items-center justify-center rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md">
+											<div className="flex flex-row">
+												<Droplet className="h-8 w-8" aria-hidden="true" />
+												<Droplets className="h-8 w-8" aria-hidden="true" />
+											</div>
+											<div className="mb-2 mt-4 text-lg font-medium">
+												Schedule for:
+											</div>
+											<p className="text-sm leading-tight text-muted-foreground">
+												Jan 11th to Jan 15th
+											</p>
 										</div>
-										<div className="mb-2 mt-4 text-lg font-medium">
-											Schedule for:
-										</div>
-										<p className="text-sm leading-tight text-muted-foreground">
-											Jan 11th to Jan 15th
-										</p>
-									</div>
+									</Link>
 								</NavigationMenuLink>
 							</li>
 
