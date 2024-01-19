cdf267e8587538460f668865235f8b2327914bb9
Kenny McGinnis <kenneth.j.mcginnis@gmail.com>
2024-01-19 14:17:10 -0700

    point esbuild directly at the correct executable

--- app/utils/mdx-bundler.server.ts
+++ app/utils/mdx-bundler.server.ts
@@ -1,4 +1,22 @@
+import path from 'path'
 import { bundleMDX } from 'mdx-bundler'
+
+if (process.platform === 'win32') {
+  process.env.ESBUILD_BINARY_PATH = path.join(
+    process.cwd(),
+    'node_modules',
+    'esbuild',
+    'esbuild.exe',
+  )
+} else {
+  process.env.ESBUILD_BINARY_PATH = path.join(
+    process.cwd(),
+    'node_modules',
+    'esbuild',
+    'bin',
+    'esbuild',
+  )
+}
 
 export async function parseMdx<FrontmatterType extends Record<string, unknown>>(mdx: string) {
 	// prettier-ignore
