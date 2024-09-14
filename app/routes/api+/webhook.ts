603af6e8e88a83f435a1ec044859a4f1f0c674e6
Kenny McGinnis <kenneth.j.mcginnis@gmail.com>
2024-09-14 12:16:00 -0700

    adding response to webhook

--- app/routes/api+/webhook.ts
+++ app/routes/api+/webhook.ts
@@ -1,4 +1,4 @@
-import { type ActionFunctionArgs } from '@remix-run/node'
+import { json, type ActionFunctionArgs } from '@remix-run/node'
 import { format } from 'date-fns'
 import { prisma } from '#app/utils/db.server'
 import { stripe } from '#server/stripe.server'
@@ -11,35 +11,39 @@ async function getStripeEvent(request: Request) {
 }
 
 export async function action({ request }: ActionFunctionArgs) {
-	const event = await getStripeEvent(request)
-	if (event.type === 'checkout.session.completed') {
-		const session = event.data.object
-		const { customer, customer_email, amount_total, payment_intent, created } = session
-		if (amount_total && customer) {
-			const transactionFee = 30
-			const processingFee = 103
-			const debit = Math.round((amount_total - transactionFee) / processingFee)
-			const { id: userId, primaryEmail } = await prisma.user.findFirstOrThrow({
-				select: { id: true, primaryEmail: true },
-				where: { stripeId: customer as string },
-			})
-			if (!primaryEmail) {
-				await prisma.user.update({
-					data: { primaryEmail: customer_email },
-					where: { id: userId },
+	try {
+		const event = await getStripeEvent(request)
+		if (event.type === 'checkout.session.completed') {
+			const session = event.data.object
+			const { customer, customer_email, amount_total, payment_intent, created } = session
+			if (amount_total && customer) {
+				const transactionFee = 30
+				const processingFee = 103
+				const debit = Math.round((amount_total - transactionFee) / processingFee)
+				const { id: userId, primaryEmail } = await prisma.user.findFirstOrThrow({
+					select: { id: true, primaryEmail: true },
+					where: { stripeId: customer as string },
+				})
+				if (!primaryEmail) {
+					await prisma.user.update({
+						data: { primaryEmail: customer_email },
+						where: { id: userId },
+					})
+				}
+				await prisma.transactions.create({
+					data: {
+						id: payment_intent as string,
+						userId,
+						debit,
+						date: format(new Date(created * 1000), 'yyyy-MM-dd'),
+						note: `Received Online PaymentId: ${payment_intent}`,
+					},
 				})
 			}
-			await prisma.transactions.create({
-				data: {
-					id: payment_intent as string,
-					userId,
-					debit,
-					date: format(new Date(created * 1000), 'yyyy-MM-dd'),
-					note: `Received Online PaymentId: ${payment_intent}`,
-				},
-			})
+			console.dir({ session }, { depth: null, colors: true })
 		}
-
-		console.dir({ session }, { depth: null, colors: true })
+		return json({ received: true })
+	} catch (error) {
+		return json({ status: 'error', error } as const, { status: 400 })
 	}
 }
