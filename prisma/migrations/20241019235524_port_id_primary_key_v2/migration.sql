5c6edb20fb608649530b2e9d18c772c7c87c62f7
Kenny McGinnis <kenneth.j.mcginnis@gmail.com>
2024-10-19 21:49:20 -0700

    table new_UserSchedule has no column named ditch

--- prisma/migrations/20241019235524_port_id_primary_key_v2/migration.sql
+++ prisma/migrations/20241019235524_port_id_primary_key_v2/migration.sql
@@ -22,7 +22,7 @@ CREATE TABLE "new_UserSchedule" (
     CONSTRAINT "UserSchedule_scheduleId_fkey" FOREIGN KEY ("scheduleId") REFERENCES "Schedule" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
     CONSTRAINT "UserSchedule_portId_fkey" FOREIGN KEY ("portId") REFERENCES "Port" ("id") ON DELETE SET NULL ON UPDATE CASCADE
 );
-INSERT INTO "new_UserSchedule" ("ditch", "hours", "scheduleId", "start", "stop", "updatedAt", "updatedBy", "userId") SELECT "ditch", "hours", "scheduleId", "start", "stop", "updatedAt", "updatedBy", "userId" FROM "UserSchedule";
+INSERT INTO "new_UserSchedule" ("portId", "hours", "scheduleId", "start", "stop", "updatedAt", "updatedBy", "userId") SELECT "portId", "hours", "scheduleId", "start", "stop", "updatedAt", "updatedBy", "userId" FROM "UserSchedule";
 DROP TABLE "UserSchedule";
 ALTER TABLE "new_UserSchedule" RENAME TO "UserSchedule";
 PRAGMA foreign_key_check;
