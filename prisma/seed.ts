ce166bc6c5dfdfee1963b49274eedb927f4f32a4
Ken McGinnis <kenneth.j.mcginnis@aexp.com>
2024-10-19 21:44:14 -0700

    reverting seed.ts

---
+++ prisma/seed.ts
@@ -0,0 +1,14 @@
+seed()
+	.catch(e => {
+		console.error(e)
+		process.exit(1)
+	})
+	.finally(async () => {
+		// await prisma.$disconnect()
+	})
+
+async function seed() {
+	console.log('🌱 Seeding...')
+	console.time(`🌱 Database has been seeded`)
+	console.timeEnd(`🌱 Database has been seeded`)
+}
