c1d37fd869801e1ea12f6a2c54f22d2e19ebf51c
Ken McGinnis <kenneth.j.mcginnis@aexp.com>
2025-04-08 15:02:30 -0700

    increasing max hours cap (temp)

--- app/components/ui/navigation-menu.tsx
+++ app/components/ui/navigation-menu.tsx
@@ -1,144 +1,100 @@
-import * as NavigationMenuPrimitive from '@radix-ui/react-navigation-menu'
-import { cva } from 'class-variance-authority'
-import { ChevronDown } from 'lucide-react'
-import * as React from 'react'
-
-import { cn } from '#app/utils/misc.tsx'
-
-const NavigationMenu = React.forwardRef<
-	React.ElementRef<typeof NavigationMenuPrimitive.Root>,
-	React.ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.Root>
->(({ className, children, ...props }, ref) => (
-	<NavigationMenuPrimitive.Root
-		ref={ref}
-		className={cn('full-w relative m-auto flex max-w-max flex-1 items-center justify-center', className)}
-		{...props}
-	>
-		{children}
-		<NavigationMenuViewport />
-	</NavigationMenuPrimitive.Root>
-))
-NavigationMenu.displayName = NavigationMenuPrimitive.Root.displayName
-
-const NavigationMenuList = React.forwardRef<
-	React.ElementRef<typeof NavigationMenuPrimitive.List>,
-	React.ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.List>
->(({ className, ...props }, ref) => (
-	<NavigationMenuPrimitive.List
-		ref={ref}
-		className={cn('group flex flex-1 list-none items-center justify-center space-x-1', className)}
-		{...props}
-	/>
-))
-NavigationMenuList.displayName = NavigationMenuPrimitive.List.displayName
-
-const NavigationMenuItem = NavigationMenuPrimitive.Item
-
-const navigationMenuTriggerStyle = cva(
-	'group inline-flex h-10 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-lg font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50',
-)
-
-const NavigationMenuTrigger = React.forwardRef<
-	React.ElementRef<typeof NavigationMenuPrimitive.Trigger>,
-	React.ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.Trigger>
->(({ className, children, ...props }, ref) => (
-	<NavigationMenuPrimitive.Trigger
-		ref={ref}
-		className={cn(navigationMenuTriggerStyle(), 'group', className)}
-		{...props}
-	>
-		{children}{' '}
-		<ChevronDown
-			className="relative top-[1px] ml-1 h-3 w-3 transition duration-200 group-data-[state=open]:rotate-180"
-			aria-hidden="true"
-		/>
-	</NavigationMenuPrimitive.Trigger>
-))
-NavigationMenuTrigger.displayName = NavigationMenuPrimitive.Trigger.displayName
-
-const NavigationMenuContent = React.forwardRef<
-	React.ElementRef<typeof NavigationMenuPrimitive.Content>,
-	React.ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.Content>
->(({ className, ...props }, ref) => (
-	<NavigationMenuPrimitive.Content
-		ref={ref}
-		className={cn(
-			'left-0 top-0 w-full data-[motion^=from-]:animate-in data-[motion^=to-]:animate-out data-[motion^=from-]:fade-in data-[motion^=to-]:fade-out data-[motion=from-end]:slide-in-from-right-52 data-[motion=from-start]:slide-in-from-left-52 data-[motion=to-end]:slide-out-to-right-52 data-[motion=to-start]:slide-out-to-left-52 md:absolute md:w-auto ',
-			className,
-		)}
-		{...props}
-	/>
-))
-NavigationMenuContent.displayName = NavigationMenuPrimitive.Content.displayName
-
-const NavigationMenuLink = NavigationMenuPrimitive.Link
-
-const NavigationMenuViewport = React.forwardRef<
-	React.ElementRef<typeof NavigationMenuPrimitive.Viewport>,
-	React.ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.Viewport>
->(({ className, ...props }, ref) => (
-	<div className={cn('absolute left-0 top-full flex justify-center')}>
-		<NavigationMenuPrimitive.Viewport
-			className={cn(
-				'origin-top-center relative mt-1.5 h-[var(--radix-navigation-menu-viewport-height)] w-full overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-lg data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-90 md:w-[var(--radix-navigation-menu-viewport-width)]',
-				className,
-			)}
-			ref={ref}
-			{...props}
-		/>
-	</div>
-))
-NavigationMenuViewport.displayName = NavigationMenuPrimitive.Viewport.displayName
-
-const NavigationMenuIndicator = React.forwardRef<
-	React.ElementRef<typeof NavigationMenuPrimitive.Indicator>,
-	React.ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.Indicator>
->(({ className, ...props }, ref) => (
-	<NavigationMenuPrimitive.Indicator
-		ref={ref}
-		className={cn(
-			'top-full z-[1] flex h-1.5 items-end justify-center overflow-hidden data-[state=visible]:animate-in data-[state=hidden]:animate-out data-[state=hidden]:fade-out data-[state=visible]:fade-in',
-			className,
-		)}
-		{...props}
-	>
-		<div className="relative top-[60%] h-2 w-2 rotate-45 rounded-tl-sm bg-border shadow-md" />
-	</NavigationMenuPrimitive.Indicator>
-))
-NavigationMenuIndicator.displayName = NavigationMenuPrimitive.Indicator.displayName
-
-const NavigationSubMenuItem = React.forwardRef<React.ElementRef<'a'>, React.ComponentPropsWithoutRef<'a'>>(
-	({ className, title, children, ...props }, ref) => {
-		return (
-			<li>
-				<NavigationMenuLink asChild>
-					<a
-						ref={ref}
-						className={cn(
-							'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
-							className,
-						)}
-						{...props}
-					>
-						<div className="text-lg font-medium leading-none">{title}</div>
-						<p className="text-l line-clamp-2 leading-snug text-muted-foreground">{children}</p>
-					</a>
-				</NavigationMenuLink>
-			</li>
+export function RenderField({
+	mainId,
+	colXl,
+	label,
+	labelClass,
+	value,
+	PopupTooltipComponent,
+	tooltip,
+	colMg,
+	colLg,
+	wrapperClass,
+}) {
+	// There are four type of offer details field:
+	//
+	//              Label       Value
+	//                |           |
+	// Example: OFFER NAME  Glasses.com
+	//
+	// 1. Normal offer detail field: Label + value
+	// 2. Tooltip for whole offer detail field: Display tooltip when we do mouse hover on either label or value
+	// 3. Icon with tooltip for label: An icon rendered on the right side of label. When we do mouse hover on the icon, the tooltip will be displayed
+	//
+	//              Icon with tooltip
+	//                     |
+	// Example: OFFER NAME© Glasses.com
+	//
+	// 4. Icon with tooltip for value: An icon rendered on the right side of value. When we do mouse hover on the icon, the tooltip will be displayed
+	//
+	//                          Icon with tooltip
+	//                                 |
+	// Example: OFFER NAME Glasses.com©
+	//
+	// PopupTooltipComponent: For value which use PopupTooltipComponent to display a popup.
+	let offerDetailsField
+	let isTooltipForWholeDetailsField =
+		tooltip && Object.keys(tooltip).length && tooltip.iconWithTooltip && tooltip.iconWithTooltip === 'field'
+	let isIconWithTooltipForValue =
+		tooltip && Object.keys(tooltip).length && tooltip.iconWithTooltip && tooltip.iconWithTooltip === 'value'
+	let isIconWithTooltipForLabel =
+		tooltip && Object.keys(tooltip).length && tooltip.iconWithTooltip && tooltip.iconWithTooltip === 'label'
+	if (isTooltipForWholeDetailsField) {
+		offerDetailsField = (
+			<span>
+				<a data-tip data-for={tooltip.tooltipId}>
+					<b className={labelClass}>{T.translate(`texts.${label}`)}</b>
+					<span className={tooltip.spanClass}>{value}</span>
+				</a>
+				<ReactTooltip id={tooltip.tooltipId} type="dark" place={tooltip.tooltipDirection} effect="solid">
+					<span>{tooltip.tooltipContent}</span>
+				</ReactTooltip>
+			</span>
 		)
-	},
-)
-NavigationSubMenuItem.displayName = 'NavigationSubMenuItem'
-
-export {
-	navigationMenuTriggerStyle,
-	NavigationMenu,
-	NavigationMenuList,
-	NavigationMenuItem,
-	NavigationSubMenuItem,
-	NavigationMenuContent,
-	NavigationMenuTrigger,
-	NavigationMenuLink,
-	NavigationMenuIndicator,
-	NavigationMenuViewport,
+	} else if (isIconWithTooltipForValue) {
+		offerDetailsField = (
+			<span>
+				<b className={labelClass}>{T.translate(`texts.${label}`)}</b>
+				<span className={tooltip.spanClass}>
+					{value}
+					<a data-tip data-for={tooltip.tooltipId}>
+						<span className={`glyph-lg ${tooltip.icon} bottom-5px padding-left-4px position-relative`}></span>
+					</a>
+					<ReactTooltip id={tooltip.tooltipId} type="dark" place={tooltip.tooltipDirection} effect="solid">
+						<span>{tooltip.tooltipContent}</span>
+					</ReactTooltip>
+				</span>
+			</span>
+		)
+	} else if (isIconWithTooltipForLabel) {
+		offerDetailsField = (
+			<span>
+				<b className={labelClass}>{T.translate(`texts.${label}`)}</b>
+				<span className={tooltip.spanClass}>
+					<a data-tip data-for={tooltip.tooltipId}>
+						<span className={`glyph-lg ${tooltip.icon} bottom-5px padding-left-4px position-relative`}></span>
+					</a>
+					<ReactTooltip id={tooltip.tooltipId} type="dark" place={tooltip.tooltipDirection} effect="solid">
+						<span>{tooltip.tooltipContent}</span>
+					</ReactTooltip>
+				</span>
+				<span className="offerDetails-Value">{value}</span>
+			</span>
+		)
+	} else {
+		offerDetailsField = (
+			<span>
+				<b className={labelClass}>{T.translate(`texts.${label}`)}</b>
+				{PopupTooltipComponent ? PopupTooltipComponent : <span className="offerDetails-Value">{value}</span>}
+			</span>
+		)
+	}
+	return (
+		<div
+			id={mainId}
+			key={mainId}
+			className={`col-md-${colMg} col-lg-${colLg} col-xl-${colXl} offer-det-row ${wrapperClass}`}
+		>
+			{offerDetailsField}
+		</div>
+	)
 }
